6894196a096b32172088f7e79483cbe4de11c9f3
tarun <24tarunkumar@gmail.com>
2023-03-09 16:21:14 +0530

    Auto Push

--- shebang.sh
+++ shebang.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#we use shebang notification at starting of script which tells sysstem kind of intrepreter to use.
-
-#shebang notification = #!/bin/bash
-#to comment a line we use # 
+# we use shebang notification at starting of script which tells sysstem kind of intrepreter to use.
+# shebang notification = #!/bin/bash
+# to comment a line we use # 
 echo Hello Tarun
