d189387b2a6c499aa36eb4db4894fe3158512659
tarun <24tarunkumar@gmail.com>
2023-03-11 11:09:21 +0530

    auto push

--- functions.sh
+++ functions.sh
@@ -1,8 +1,9 @@
 echo " i am executing stat function which holds info regarding load average , today date and number of sessions opened "
 
 stat() {
-echo "Today date is : $(date +%D)"
-echo "load average is $(uptime | awk -F : '{print $4}' | awk -F , '{print $NF}')"    
+echo "Today date is : $(date +%D)
+echo "load average is : $(uptime | awk -F : '{print $4}' | awk -F , '{print $1}')"
+echo " Total number of sessions opened today : $(who | wc -l)
 }
 
 stat
