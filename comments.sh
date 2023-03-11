6be5ce204fa8638d776874568ed04b8a59967f25
tarun <24tarunkumar@gmail.com>
2023-03-11 14:48:51 +0530

    auto push

--- comments.sh
+++ comments.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo " we are demonostrating multiline comments way for we we have to follow below format like <<COMMENT at the end COMMENT"
+echo " we are demonostrating multiline comments way for that we have to follow below format like <<COMMENT at the starting from where you want to comment 
+at the end COMMENT to end comment . we use # to comment single line"
 <<COMMENT
 a=10
 echo "value is $a
