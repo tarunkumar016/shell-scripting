fea4fa77609203fa96b397cd6f09a8a47ca9c4de
tarun <24tarunkumar@gmail.com>
2023-03-11 14:07:31 +0530

    auto push

--- case.sh
+++ case.sh
@@ -1,12 +1,12 @@
 Action=$1
 case $Action in 
 start)) 
-echo " Payment service is being started"
+echo "Payment service is being started"
 ;;
 stop))
 echo "Payment service being stopped"
 ;;
 restart))
-echo  "payment services are restarted"
+echo "payment services are restarted"
 ;;
 esac
