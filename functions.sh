5f77fa8e9f1a394b685f21bdc9b91cf1932ec794
tarun <24tarunkumar@gmail.com>
2023-03-11 10:20:02 +0530

    auto push

--- functions.sh
+++ functions.sh
@@ -4,7 +4,7 @@ sample() {
 stat() {
 echo "Today date is : $(date +%D)"
 echo "Total number of sessions : $(who | wc -l)"
-echo "Load average is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $2}')
+echo "Load average is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $2}')"
  }
 echo "calling stat function"
 stat
